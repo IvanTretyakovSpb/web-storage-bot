9b717acf41e4a097f4a660f274e419c32a9b2ff9
Tretyakov Ivan <timmitarh@mail.ru>
2024-07-28 22:16:29 +0300

    Completed stage #2

--- dispatcher/src/main/java/com/tretyakov/DispatcherApplication.java
+++ dispatcher/src/main/java/com/tretyakov/DispatcherApplication.java
@@ -1,4 +1,4 @@
-package com.tretykov;
+package com.tretyakov;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- dispatcher/src/main/java/com/tretyakov/controller/TelegramBot.java
+++ dispatcher/src/main/java/com/tretyakov/controller/TelegramBot.java
@@ -1,4 +1,4 @@
-package com.tretykov.controller;
+package com.tretyakov.controller;
 
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +9,8 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import javax.annotation.PostConstruct;
+
 @Component
 @Log4j
 public class TelegramBot extends TelegramLongPollingBot {
@@ -17,7 +19,16 @@ public class TelegramBot extends TelegramLongPollingBot {
     private String botName;
     @Value("${bot.token}")
     private String botToken;
+    private final UpdateController updateController;
 
+    public TelegramBot(UpdateController updateController) {
+        this.updateController = updateController;
+    }
+
+    @PostConstruct
+    public void init() {
+        updateController.registerBot(this);
+    }
 
     @Override
     public String getBotUsername() {
@@ -31,13 +42,7 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        Message originalMessage = update.getMessage();
-        log.debug(originalMessage.getText());
-
-        SendMessage response = new SendMessage();
-        response.setChatId(originalMessage.getChatId().toString());
-        response.setText("Hello from bot!");
-        sendAnswerMessage(response);
+        updateController.processUpdate(update);
     }
 
     public void sendAnswerMessage(SendMessage message) {
