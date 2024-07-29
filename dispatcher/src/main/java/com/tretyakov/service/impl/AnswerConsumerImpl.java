951fc828b9b9aeef7c7ce2df1e722e3e43d18dff
Tretyakov Ivan <timmitarh@mail.ru>
2024-07-29 10:30:30 +0300

    Completed stage #4

---
+++ dispatcher/src/main/java/com/tretyakov/service/impl/AnswerConsumerImpl.java
@@ -0,0 +1,24 @@
+package com.tretyakov.service.impl;
+
+import com.tretyakov.controller.UpdateController;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+import static com.tretyakov.model.RabbitQueue.ANSWER_MESSAGE;
+
+@Service
+public class AnswerConsumerImpl implements com.tretyakov.service.AnswerConsumer {
+
+    private final UpdateController updateController;
+
+    public AnswerConsumerImpl(UpdateController updateController) {
+        this.updateController = updateController;
+    }
+
+    @Override
+    @RabbitListener(queues = ANSWER_MESSAGE)
+    public void consume(SendMessage sendMessage) {
+        updateController.setView(sendMessage);
+    }
+}
